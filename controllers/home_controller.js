6d8d632df4a90b6af2baae5229e79c958260793e
CodeHater143 <ay75143@gmail.com>
2019-08-21 19:25:02 +0530

    view engine setup

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,6 @@
 module.exports.home = function(req,res){
-    return res.end('<h1>hello express user</h1>');
+    //return res.end('<h1>hello express user</h1>');
+    return res.render('home',{
+        title:"Home"
+    });
 }
